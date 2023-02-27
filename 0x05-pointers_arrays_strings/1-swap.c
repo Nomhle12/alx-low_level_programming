1df0fcab1e61ed9028268b6c851a09c415f892ba
Nomhle12 <nomhlemabena12@gmail.com>
2023-02-27 05:01:18 -0800

    Task 2 swap two pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps the values of two integers
+ *@a: interger to be swaped
+ *@b: interger to be swaped
+ *
+ *Return: on succes 0.
+ **/
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
